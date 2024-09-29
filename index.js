f201b587950768a29c86f0dc178c5e60d0dd0724
AkashMala <135145391+AkashMala@users.noreply.github.com>
2024-09-29 12:28:26 +0530

    Profile-Statistics

---
+++ index.js
@@ -0,0 +1,24 @@
+const countersE1=document.querySelectorAll(".counter");
+
+
+
+countersE1.forEach(counterE1=>{
+    counterE1.innerText="0";
+
+    incrementCounter();
+    function incrementCounter(){
+   let  currentNum = +counterE1.innerText;
+   const  dataCeil=counterE1.getAttribute("data-ceil");
+  const increment=dataCeil / 15
+  currentNum=Math.ceil(currentNum + increment)
+  counterE1.innerText=currentNum
+
+  if(currentNum < dataCeil){
+    counterE1.innerText=currentNum
+    setTimeout
+    (incrementCounter, 100)
+  }else{
+    counterE1.innerText=dataCeil
+  }
+    }
+});
